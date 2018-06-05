7950e965b996eb776f3b81c5f370f1b4b6b089f4
WZZ-HOSPITAL <WZZ-HOSPITAL@DESKTOP-92STND0>
2018-06-05 11:50:50 +0800

    play over

--- ConsoleApp2/Player.cs
+++ ConsoleApp2/Player.cs
@@ -4,8 +4,20 @@ using System.Text;
 
 namespace ConsoleApp2
 {
-    class Player
+    public class Player
     {
+        public string FirstName { get; set; }
+        public int Play(string name)
+        {
+            this.FirstName = name;
+            switch (name)
+            {
+                case "石头": return 1;
+                case "剪刀": return 2;
+                case "布": return 3;
+                default: return 0;
+            }
 
+        }
     }
 }
