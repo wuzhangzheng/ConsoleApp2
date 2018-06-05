01d7f67deb4bc3d677ba3d6648a81ba4f23e3f33
WZZ-HOSPITAL <WZZ-HOSPITAL@DESKTOP-92STND0>
2018-06-05 11:01:27 +0800

    1

---
+++ ConsoleApp2/Computer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Computer
+    {
+        public string Name { get; set; }
+        public int Play()
+        {
+            Random r = new Random();
+            var num = r.Next(1, 4);
+            switch (num)
+            {
+                case 1:Name = "石头";break;
+                case 2:Name = "剪刀";break;
+                case 3: Name = "布";break;
+            }
+            return num;
+        }
+    }
+}
